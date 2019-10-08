989b072b2edce60c4529060489bc378085f77581
Peter Ayeni <ayenicology@gmail.com>
2019-10-08 12:40:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,12 @@
 class Dog 
-  attr_accessor :this_dogs_name
-  # Setter
+  
   def name=(dog_name)
-    self.this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   # Getter
   def name
-    self.this_dogs_name
+    @this_dogs_name
   end
 class
 
